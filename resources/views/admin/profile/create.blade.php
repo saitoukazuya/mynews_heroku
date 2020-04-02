5ace8c5e3d3847566b193d2b13b6de3d7474bf9d
saitoukazuya <qz.14.kz@gmail.com>
2020-04-02 22:00:29 +0000

    10_2

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8">
+        <meta http-equiv="X-UA-compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <titale>profile</titale>
+    </head>
+    <body>
+        <h1>プロフィール作成画面</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="uft-8">
+       <meta http-equiv="X-UA-compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+       <titale>edit</titale>
+    </head>
+    <body>
+        <h1>編集画面</h1>
+    </body>
+</html>
