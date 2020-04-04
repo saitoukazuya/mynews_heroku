3db81167d6deb0d1556834fbf7ac0c17b48f5112
saitoukazuya <qz.14.kz@gmail.com>
2020-04-04 10:26:41 +0000

    11

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -13,3 +13,20 @@
 </html>
 
 
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h1>ニュース新規作成</h1>
+            </div>
+        </div>
+    </div>
+@endsection
