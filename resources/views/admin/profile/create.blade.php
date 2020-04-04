252f56894125b8123e7cd5cb6b87501afde30cce
saitoukazuya <qz.14.kz@gmail.com>
2020-04-04 12:18:20 +0000

    11_3

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="uft-8">
-        <meta http-equiv="X-UA-compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>profile</title>
+        <title>My profile</title>
     </head>
     <body>
-        <h1>プロフィール</h1>
+        <h1>My プロフィール</h1>
     </body>
 </html>
 
 
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
 
 
-{{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+{{-- admin.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
 @section('title', 'Myプロフィール')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
