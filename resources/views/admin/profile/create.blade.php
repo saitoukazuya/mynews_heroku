14f20a82ac849a333377e5f99bb9900ba0a0da26
saitoukazuya <qz.14.kz@gmail.com>
2020-04-04 11:41:27 +0000

    11_2

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,28 @@
         <meta http-equiv="X-UA-compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <titale>profile</titale>
+        <title>profile</title>
     </head>
     <body>
-        <h1>プロフィール作成画面</h1>
+        <h1>プロフィール</h1>
     </body>
 </html>
+
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h1>Myプロフィール</h1>
+            </div>
+        </div>
+    </div>
+@endsection
