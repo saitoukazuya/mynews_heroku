3cf3acf57bf7d96f95b18223f9550aa29b78dc62
saitoukazuya <qz.14.kz@gmail.com>
2020-04-06 09:27:38 +0000

    11_4

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'],function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 //09課題　「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
 //routes::group(['prefix' => 'XXX'],function() {
@@ -31,3 +31,8 @@ Route::group(['prefix' => 'admin'],function() {
 Route::group(['prefix' => 'admin'],function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
