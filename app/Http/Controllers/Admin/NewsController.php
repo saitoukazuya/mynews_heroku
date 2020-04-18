a128aadf7d58347aef497a8365c887a6f919d657
saitoukazuya <qz.14.kz@gmail.com>
2020-04-18 12:18:27 +0000

    4/18_2

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+use App\News;
 
 class NewsController extends Controller
 {
@@ -14,7 +15,24 @@ class NewsController extends Controller
     
     public function create(Request $request)
     {
-        return redirect('admin/news/create');
+       $this->validate($request, News::$rules);
+       
+       $news = new News;
+       $form = $request->all();
+       
+       if (isset($form['image'])) {
+           $parh = $request->file('image')->store('public/image');
+           $news->image_path =basename($path);
+       } else {
+           $news->image_path = null;
+       }
+       unset($form['_token']);
+       unset($form['image']);
+       
+       $news->fill($form);
+       $news->save();
+       
+       return redirect('admin/news/create');
     }
     
 }
